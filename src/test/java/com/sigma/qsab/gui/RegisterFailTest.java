5d119605c78d8f6e016c65920523d1f2b025f8f0
Thomas Sundberg <tsu@kth.se>
2011-07-06 09:42:45 +0200

    Increased abstraction level

--- src/test/java/com/sigma/qsab/gui/RegisterFailTest.java
+++ src/test/java/com/sigma/qsab/gui/RegisterFailTest.java
@@ -1,23 +1,20 @@
 package com.sigma.qsab.gui;
 
-//Requires Fest library:
 import com.sigma.qsab.QSAB;
 import org.fest.swing.core.EmergencyAbortListener;
 import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.fixture.FrameFixture;
-
-//Requires JUnit 4.1 library:
 import org.junit.After;
-import org.junit.Test;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class RegisterFailTest {
 
     private EmergencyAbortListener listener;
-    private FrameFixture window;    
+    private FrameFixture window;
     private RegisterLibrary lib;
 
     @BeforeClass
@@ -32,22 +29,38 @@ public class RegisterFailTest {
             protected MainFrame executeInEDT() {
                 return (new QSAB()).getMainFrame();
             }
-        });        
+        });
         window = new FrameFixture(frame);
-        listener = EmergencyAbortListener.registerInToolkit();   
+        listener = EmergencyAbortListener.registerInToolkit();
         lib = new RegisterLibrary(window);
-        
+    }
+
+    @After
+    public void tearDown() {
+        listener.unregister();
+        window.cleanUp();
     }
 
     @Test
-    public void testRegisterFillOut() throws InterruptedException {
-        //Välj personnummerfel
+    public void testRegisterFillOut() {
+        fillOutForm();
+
+        lib.clickButton("register_next");
+        pause(5000);
+
+        assertForm();
+    }
+
+    private void findForm() {
         lib.clickButton("welcome_login");
-        window.list("superadmin_glitchlist").clickItem("Formateringsfel i personnummer");          
+        window.list("superadmin_glitchlist").clickItem("Formateringsfel i personnummer");
         lib.clickButton("superadmin_accept");
-        
-        //Fyll i fält
         lib.clickButton("welcome_register");
+    }
+
+    private void fillOutForm() {
+        findForm();
+
         lib.fillInName("Test", "Testsson");
         lib.fillInSocialID("1212-121212");
         lib.fillInAddress("Testvägen 55", "12345", "Testort");
@@ -55,22 +68,23 @@ public class RegisterFailTest {
         lib.fillInCellPhoneNumber("073-1234567");
         lib.fillInEmail("test@test.com");
         lib.fillInPasswordTwice("123456");
-        lib.clickButton("register_next");
-        //Thread.sleep(5000);
-        
-        //Kontrollera det vi fyllde i
+    }
+
+    private void assertForm() {
         lib.assertName("Test", "Testsson");
         lib.assertSocialID("1212-121212");
         lib.assertAddress("Testvägen 55", "12345", "Testort");
         lib.assertPhoneNumber("08-123456");
         lib.assertCellPhoneNumber("073-1234567");
         lib.assertEmail("test@test.com");
-        lib.assertPasswords("123456");                  
+        lib.assertPasswords("123456");
     }
 
-    @After
-    public void tearDown() {
-        listener.unregister();
-        window.cleanUp();
+    private void pause(long timeToSleep) {
+        try {
+            Thread.sleep(timeToSleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
