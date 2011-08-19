c5138b4954d7d027bc2c55079f2d47c58e74dd44
Jonas Frogvall <frjo02@kth.se>
2011-08-19 13:24:16 +0200

    Namnbyten för bättre översikt.

---
+++ implementation/src/main/java/com/sigma/qsab/glitches/customglitches/Login_IgnoreCaseForComparingPasswordsGlitch.java
@@ -0,0 +1,21 @@
+package com.sigma.qsab.glitches.customglitches;
+
+import com.sigma.qsab.glitches.Glitch;
+
+public class Login_IgnoreCaseForComparingPasswordsGlitch extends Glitch {
+
+    private static String shortDesc = "Kontrollera inte versaler och gemener i l\u00f6senord vid login";
+    private static String longDesc = "Kontrollerar att l\u00f6senord \u00f6verensst\u00e4mmer vid "
+            + "login, men g\u00f6r inte skillnad p\u00e5 versaler och gemener.";
+
+    public Login_IgnoreCaseForComparingPasswordsGlitch() {
+        super(shortDesc, longDesc, REGISTER_AREPASSWORDSEQUALGLITCH);
+    }
+
+    @Override
+    public Object performGlitch(Object... args) {
+        String password = (String)args[0];
+        String passwordRepeat = (String)args[1];
+        return password.equalsIgnoreCase(passwordRepeat);
+    }
+}
---
+++ implementation/src/main/java/com/sigma/qsab/glitches/customglitches/Registration_DoNotCheckIfSocialIDIsCorrectGlitch.java
@@ -0,0 +1,21 @@
+package com.sigma.qsab.glitches.customglitches;
+
+import com.sigma.qsab.glitches.Glitch;
+
+public class Registration_DoNotCheckIfSocialIDIsCorrectGlitch extends Glitch {
+
+    private static String shortDesc = "Kontrollera inte personnummer alls";
+    private static String longDesc = "Kontrollerar inte huruvida personnummer "
+            + "\u00e4r ifyllt \u00f6ver huvud taget. \u00c4r f\u00e4ltet betvingat s\u00e5 kan det "
+            + "fortfarande g\u00e4lla att blanka str\u00e4ngar inte g\u00e5r igenom, beroende "
+            + "p\u00e5 val av andra fel.";
+
+    public Registration_DoNotCheckIfSocialIDIsCorrectGlitch() {
+        super(shortDesc, longDesc, REGISTER_SOCIALIDGLITCH);
+    }
+
+    @Override
+    public Object performGlitch(Object... args) {
+        return true;
+    }
+}
