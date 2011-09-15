01b88a487ea3d5bd9495dd4279ac7a9b74a12206
ext.jonas.frogvall <ext.jonas.frogvall@WL00188.sigma.local>
2011-09-15 15:22:51 +0200

    Uppdelning i moduler. Parameteriserad körning (som dock inte fungerar helt pga EDT violation).

---
+++ test-library/gui-drivers/sikuli/src/test/java/com/sigma/qsab/gui/runner/SikuliGUIRunner.java
@@ -0,0 +1,194 @@
+package com.sigma.qsab.gui.runner;
+
+import com.sigma.qsab.QSAB;
+import com.sigma.qsab.gui.GUIStrings;
+import com.sigma.qsab.gui.MainFrame;
+import java.io.IOException;
+import java.net.URL;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
+import org.sikuli.script.Settings;
+
+public class SikuliGUIRunner extends GUIRunner {    
+    
+    private static final double minimumSimilarityScore = 0.9;
+    private static final boolean showActions = false;
+    private Screen screen;
+    private MainFrame mainFrame;
+    private GUIStrings gs;
+    
+    @SuppressWarnings("CallToThreadDumpStack")
+    public SikuliGUIRunner() {
+        try {
+            gs = new GUIStrings("/strings.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void assertAddress(String street, String zipcode, String city) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertCellPhoneNumber(String cellPhoneNumber) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertEmail(String email) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertName(String firstname, String lastname) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertPasswords(String password) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertPhoneNumber(String phoneNumber) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void assertSocialID(String socialID) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void clickButton(String buttonText) {
+        if (successfullyClickedButton(buttonText)) {
+            return;
+        }
+        if (successfullyClickedButton(buttonText + " focused")) {
+            return;
+        }
+        if (successfullyClickedButton(buttonText + " mouseover")) {
+            return;
+        }
+        if (successfullyClickedButton(buttonText + " focused mouseover")) {
+            return;
+        }
+        //Nothing found, probably should do something.
+    }
+    
+    @Override
+    public void fillInAddress(String street, String zipcode, String city) {
+        try {
+            screen.type(fullPath("Postadress"), street, 0);
+            screen.type(fullPath("Postnummer"), zipcode, 0);
+            screen.type(fullPath("Ort"), city, 0);
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }
+    }
+    
+    @Override
+    public void fillInCellPhoneNumber(String cellPhoneNumber) {
+        try {
+            screen.type(fullPath("Mobil"), cellPhoneNumber, 0);
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void fillInEmail(String email) {
+        try {
+            screen.paste(fullPath("E-postadress"), email);            
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void fillInName(String firstname, String lastname) {
+        try {
+            screen.type(fullPath("Förnamn"), firstname, 0);            
+            screen.type(fullPath("Efternamn"), lastname, 0);
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void fillInPasswordTwice(String password) {
+        try {
+            screen.type(fullPath("Lösenord"), password, 0);            
+            screen.type(fullPath("Upprepa lösenord"), password, 0);
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void fillInPhoneNumber(String phoneNumber) {
+        try {
+            screen.type(fullPath("Hemtelefon"), phoneNumber, 0);
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }
+    }
+    
+    @Override
+    public void fillInSocialID(String socialID) {
+        try {
+            screen.type(fullPath("Personnummer"), socialID, 0);            
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void initiate() {
+        screen = new Screen();
+        mainFrame = new QSAB().getMainFrame();
+        Settings.MinSimilarity = minimumSimilarityScore;
+        Settings.ShowActions = showActions;
+    }
+    
+    @Override
+    public void login(String socialID, String password) {
+        try {
+            screen.type(fullPath("Login Personnummer"), socialID, 0);
+            screen.type(fullPath("Login Lösenord"), password, 0);            
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void selectItemFromGlitchList(String item) {
+        try {
+            screen.click(fullPath(item), 0);            
+        } catch (FindFailed ff) {
+            //Do nothing.
+        }        
+    }
+    
+    @Override
+    public void tearDown() {
+        mainFrame.dispose();
+    }
+    
+    private String fullPath(final String text) {
+        String fileName = text + ".png";
+        final URL url = ClassLoader.getSystemResource(fileName);
+        return url.toExternalForm();//.getPath();
+    }
+    
+    private boolean successfullyClickedButton(String buttonFileName) {
+        try {
+            screen.click(fullPath(buttonFileName), 0);
+            return true;
+        } catch (FindFailed ff) {
+            return false;
+        }
+    }
+}
---
+++ test-library/test-drivers/junit/src/test/java/com/sigma/qsab/gui/RegisterTest.java
@@ -0,0 +1,171 @@
+package com.sigma.qsab.gui;
+
+import com.sigma.qsab.data.Customer;
+import com.sigma.qsab.data.CustomerStorer;
+import com.sigma.qsab.data.CustomerStorerImpl;
+import com.sigma.qsab.gui.runner.FESTGUIRunner;
+import com.sigma.qsab.gui.runner.GUIRunner;
+import com.sigma.qsab.gui.runner.SikuliGUIRunner;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class RegisterTest {
+
+    private static GUIRunner runner;
+    private String firstName;
+    private String lastName;
+    private String socialID;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String phone;
+    private String cellPhone;
+    private String email;
+    private String password;
+
+    /*@BeforeClass
+    public static void setUpOnce() {
+        runner = new FESTGUIRunner();        
+    }*/
+
+    @Before
+    public void setUp() {
+        runner.initiate();                  
+    }
+
+    @After
+    public void tearDown() {
+        runner.tearDown();
+        cleanCustomersData();     
+    }
+
+    public RegisterTest(GUIRunner runner) {
+        RegisterTest.runner = runner;
+    }
+
+    @Test
+    public void testRegisterFillOut() {
+        findForm();
+        prepareStrings();
+        fillOutForm();
+
+        runner.clickButton("Nästa");
+        runner.pause(1000);
+
+        assertForm();
+        
+        runner.clickButton("Föregående");
+        runner.clickButton("Föregående");
+        runner.clickButton("Logga in");
+        
+        login();
+        
+        runner.clickButton("Logga in");
+        
+        runner.pause(3000);
+    }
+
+    @Test
+    public void testFailRegisterFillOut() {
+        selectGlitch();
+        findForm();
+        prepareErroneousStrings();
+        fillOutForm();
+
+        runner.clickButton("Nästa");
+        runner.pause(1000);
+
+        assertForm();
+        
+        runner.clickButton("Föregående");
+        runner.clickButton("Föregående");
+        runner.clickButton("Logga in");
+        
+        login();
+        
+        runner.clickButton("Logga in");
+        
+        runner.pause(3000);
+    }
+
+    private void selectGlitch() {
+        runner.clickButton("Välj buggar");
+        runner.selectItemFromGlitchList("Formateringsfel i personnummer");
+        runner.clickButton("Applicera valda fel");
+    }
+
+    private void findForm() {
+        runner.clickButton("Ny kund");
+    }
+
+    private void prepareStrings() {
+        firstName = "Test";
+        lastName = "Testsson";
+        socialID = "121212-1212";
+        street = "Testgatan 55";
+        zipCode = "12345";
+        city = "Testort";
+        phone = "08-123456";
+        cellPhone = "073-123456";
+        email = "test@test.com";
+        password = "123456";
+    }
+
+    private void prepareErroneousStrings() {
+        prepareStrings();
+        socialID = "1212-121212";        
+    }
+
+    private void fillOutForm() {
+        runner.fillInName(firstName, lastName);
+        runner.fillInSocialID(socialID);
+        runner.fillInAddress(street, zipCode, city);
+        runner.fillInPhoneNumber(phone);
+        runner.fillInCellPhoneNumber(cellPhone);
+        runner.fillInEmail(email);
+        runner.fillInPasswordTwice(password);
+    }
+
+    private void assertForm() {
+        runner.assertName(firstName, lastName);
+        runner.assertSocialID(socialID);
+        runner.assertAddress(street, zipCode, city);
+        runner.assertPhoneNumber(phone);
+        runner.assertCellPhoneNumber(cellPhone);
+        runner.assertEmail(email);
+        runner.assertPasswords(password);
+    }
+
+    private void login() {
+        runner.login(socialID, password);
+    }
+
+    private void cleanCustomersData() {
+        prepareStrings();
+        CustomerStorer customers = new CustomerStorerImpl();
+        Customer customer = new Customer(firstName, lastName, socialID, street, zipCode, city, phone, cellPhone, email, password);
+        if (customers.deleteCustomer(customer))
+            System.out.println("Deleted customer:\n" + customer);
+        prepareErroneousStrings();
+        customer = new Customer(firstName, lastName, socialID, street, zipCode, city, phone, cellPhone, email, password);
+        if (customers.deleteCustomer(customer))
+            System.out.println("Deleted customer:\n" + customer);
+    }
+    
+    @Parameterized.Parameters
+    public static Collection<GUIRunner[]> runners() {
+        List<GUIRunner[]> runners = new LinkedList<GUIRunner[]>();
+        //runners.add(new GUIRunner[]{new SikuliGUIRunner()});
+        runners.add(new GUIRunner[]{new FESTGUIRunner()});        
+
+        return runners;
+    }
+}
